434928fee72191482e17165bdde3c93cd3f9f015
leandromenoci <menoci.leandro@gmail.com>
2022-08-17 22:45:45 -0300

    GridTwoColumn

---
+++ src/components/GridTwoColumn/index.jsx
@@ -0,0 +1,29 @@
+import P from 'prop-types';
+import * as Styled from './styles';
+import { SectionBackgroud } from '../SectionBackgroud';
+import { Heading } from '../Heading';
+import { TextComponent } from '../TextComponent';
+
+export const GridTwoColumn = ({ title, text, srcImg, background = false }) => {
+  return (
+    <SectionBackgroud background={background}>
+      <Styled.Container background={background}>
+        <Styled.TextContainer>
+          <Heading uppercase colorDark={!background}>
+            {title}
+          </Heading>
+          <TextComponent>{text}</TextComponent>
+        </Styled.TextContainer>
+        <Styled.ImageContainer>
+          <Styled.Image src={srcImg} alt={title} />
+        </Styled.ImageContainer>
+      </Styled.Container>
+    </SectionBackgroud>
+  );
+};
+GridTwoColumn.propTypes = {
+  title: P.string.isRequired,
+  text: P.string.isRequired,
+  srcImg: P.string.isRequired,
+  background: P.bool,
+};
---
+++ src/components/GridTwoColumn/mock.js
@@ -0,0 +1,5 @@
+export default {
+  title: 'Grid tow columns',
+  text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. A reprehenderit ex consequatur nesciunt, quo alias ratione consectetur dignissimos, enim magni, odio voluptates voluptatum quaerat ipsa adipisci minima laudantium soluta natus!`,
+  srcImg: 'assets/images/javascript.svg',
+};
