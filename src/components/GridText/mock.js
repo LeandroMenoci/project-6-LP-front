4cbc0515d888af0705e0add933c93e6bc6fca559
leandromenoci <menoci.leandro@gmail.com>
2022-08-18 17:53:08 -0300

    Grid text

---
+++ src/components/GridText/mock.js
@@ -0,0 +1,23 @@
+export default {
+  background: false,
+  title: 'My Grid',
+  description:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam fugiat provident, qui sequi est ad, eos',
+  grid: [
+    {
+      title: 'Teste 1',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, nesciunt! Exercitationem provident est voluptas error, debitis mollitia ipsa voluptatibus optio asperiores praesentium fugit consequatur accusantium nisi, non a nihil sunt?',
+    },
+    {
+      title: 'Teste 2',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, nesciunt! Exercitationem provident est voluptas error, debitis mollitia ipsa voluptatibus optio asperiores praesentium fugit consequatur accusantium nisi, non a nihil sunt?',
+    },
+    {
+      title: 'Teste 3',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, nesciunt! Exercitationem provident est voluptas error, debitis mollitia ipsa voluptatibus optio asperiores praesentium fugit consequatur accusantium nisi, non a nihil sunt?',
+    },
+  ],
+};
